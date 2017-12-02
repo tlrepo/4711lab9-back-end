db1c04e6710fdb0f70a1ea8891a9e753aec35163
Daniel <daniel51_1994@hotmail.com>
2017-12-01 20:25:06 -0800

    Create Job controller

--- application/controllers/Job.php
+++ application/controllers/Job.php
@@ -1,12 +1,47 @@
 <?php
+require APPPATH . '/third_party/restful/libraries/Rest_controller.php';
+
 /**
- * Created by PhpStorm.
- * User: danie
- * Date: 2017-12-01
- * Time: 7:54 PM
+ * Class Job
+ *
+ * @author Daniel
  */
+class Job extends Rest_Controller {
+    function __construct(){
+        parent::__construct();
+        $this->load->model('Tasks');
+    }
 
-class Job
-{
+    // Handle an incoming GET ... return a menu item or all of them
+    function index_get($key=null) {
+        if (!$key) {
+            $this->response($this->tasks->all(), 200);
+        } else {
+            $result = $this->tasks->get($key);
+            if ($result != null)
+                $this->response($result, 200);
+            else
+                $this->response(array('error' => 'Todo item not found!'), 404);
+        }
+    }
 
+    // Handle an incoming PUT - update a todo item
+    function index_put($key=null) {
+        $record = array_merge(array('id' => $key), $this->_put_args);
+        $this->tasks->update($record);
+        $this->response(array('ok'), 200);
+    }
+
+    // Handle an incoming POST - add a new todo item
+    function index_post($key=null) {
+        $record = array_merge(array('id' => $key), $_POST);
+        $this->tasks->add($record);
+        $this->response(array('ok'), 200);
+    }
+
+    // Handle an incoming DELETE - delete a todo item
+    function index_delete($key=null) {
+        $this->tasks->delete($key);
+        $this->response(array('ok'), 200);
+    }
 }
